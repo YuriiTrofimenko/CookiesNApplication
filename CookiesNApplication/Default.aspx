c0856edf72b81ed6293151d69f3c6884b217abb9
Yurii Trofimenko <tyaa@ukr.net>
2018-07-15 18:28:22 +0300

    init

---
+++ CookiesNApplication/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CookiesNApplication.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="/wwwroot/style.css" />
+</head>
+<body>
+    <form id="form1" runat="server" method="get">
+        <div runat="server" id="subscriptionForm">
+            <label for="name">Name:</label>
+            <input id="name" name="name" placeholder="Your name" />
+            <label for="email">E-mail:</label>
+            <input id="email" name="email" placeholder="Your email" />
+            <input id="lastname" name="lastname" placeholder="Lastname"/>
+            <asp:Button runat="server" ID="submit" OnClick="submit_Click" type="submit" Text="Subscribe" />
+        </div>
+        <div runat="server" id="subscriptionComlete">
+            <span>Subscription is comleted</span>
+        </div>
+        <div>
+            <span>Hello World!</span>
+        </div>
+        <div>
+            <span>Success count: </span><span><%= mCount%></span>
+        </div>
+        <div>
+            <span>Total count: </span><span><%= mTotalCount%></span>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CookiesNApplication/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CookiesNApplication
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public static int mCount = 0;
+        public static int mTotalCount = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            subscriptionComlete.Visible = false;
+            //читаем куки
+            HttpCookie cookie = Request.Cookies["Preferences"];
+            string subscription;
+            if (cookie != null)
+            {
+                subscription = cookie["subscription"];
+                if (subscription != null && subscription == "true")
+                {
+                    subscriptionForm.Visible = false;
+                }
+            }
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            mTotalCount++;
+            if (this.Request.Params["lastname"] == "")
+            {
+                mCount++;
+                //...
+                subscriptionForm.Visible = false;
+                subscriptionComlete.Visible = true;
+                // Создать объект cookie-набора.
+                HttpCookie cookie = new HttpCookie("Preferences");
+                // Установить значение в нем.
+                cookie["subscription"] = "true";
+                // Добавить в него еще одно значение.
+                //cookie["Country"] = "US";
+                //Хранить 1 год
+                cookie.Expires = DateTime.Now.AddYears(1);
+                // Добавить cookie-набор к текущему веб-ответу.
+                Response.Cookies.Add(cookie);
+            }
+            else {
+
+            }
+        }
+    }
+}
